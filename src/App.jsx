829a610e028466c12db9e73713f5f037180df5ad
Lydia Eluro <elurolydia@gmail.com>
2022-08-20 04:43:42 +0800

    added responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Card from './components/Card';
+import data from './components/data'
 
 function App() {
+  const elements = data.map(item => {
+    return (
+        <Card
+          key = {item.id}
+          img = {item.image}
+          country = {item.country}
+          location = {item.location}
+          date = {item.date}
+          desc = {item.description}
+        />
+    )
+  }) 
+  
   return (
-    <div>
+    <div id='body'>
       <Navbar />
-      <Card />
+      {elements}
     </div>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,23 +1,18 @@
 import React from "react";
 import locationIcon from "./images/locationIcon.png";
-import fuji from "./images/fuji.png";
 
 
 
 
-export default function Card () {
+export default function Card (props) {
     return (
         <div className="card">
-            <img src={fuji} alt="location picture"  id="locationImg"/>
+            <img src={props.img} alt="location picture"  id="locationImg"/>
             <section>
-                <p className="locationName"><img src={locationIcon} alt="Location Icon" id="icon"/> JAPAN <span id="view">View on Google Maps</span></p>
-                <h1>Mount Fuji</h1>
-                <h6>12 Jan, 2021 - 24 Jan, 2021</h6>
-                <p id="description">Mount Fuji is the tallest mountain in Japan, 
-                    standing at 3,776 meters (12,380 feet). 
-                    Mount Fuji is the single most popular tourist site in Japan, 
-                    for both Japanese and foreign tourists.
-                </p>
+                <p className="locationName"><img src={locationIcon} alt="Location Icon" id="icon"/> {props.country} <span id="view">View on Google Maps</span></p>
+                <h1>{props.location}</h1>
+                <h6>{props.date}</h6>
+                <p id="description"> {props.desc} </p>
             </section>
         </div>
     )
